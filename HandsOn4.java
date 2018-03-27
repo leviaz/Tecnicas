30b9549c3cefeb648e56fc5d67899c7b801643ce
leviaz <37335495+leviaz@users.noreply.github.com>
2018-03-27 13:08:29 -0300

    Update HandsOn4.java

--- HandsOn4.java
+++ HandsOn4.java
@@ -2,10 +2,12 @@ class HandsOn4{
 	public static void main(String[] args){
 		Sistema s = new Sistema();
 		s.criarContaXPTOBasic(1);
-		s.creditar(0,1000);
-		s.consultarSaldo(0);
-		s.criarContaXPTOExtreme(2);
 		s.creditar(1,1000);
+		s.debitar(1,100);
 		s.consultarSaldo(1);
+		s.criarContaXPTOExtreme(2);
+		s.creditar(2,1000);
+		s.debitar(2,100);
+		s.consultarSaldo(2);
 	}
 }
