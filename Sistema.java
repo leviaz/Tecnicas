ce5aa314fdbf5c324f0879e876ca199f7c4d3d24
leviaz <37335495+leviaz@users.noreply.github.com>
2018-03-27 13:07:57 -0300

    Update Sistema.java
    adicionado metodo de busca id

--- Sistema.java
+++ Sistema.java
@@ -27,11 +27,41 @@ class Sistema{
 		this.contas[this.getIndice()]=c;
 		this.setIndice(this.getIndice()+1);
 	}
+	public Conta buscar(int i){
+		Conta c;
+		for(int j = 0; j<=this.indice-1;j++){
+			//System.out.println(this.contas[j].getId());
+			if(this.contas[j].getId()==i){
+				c=this.contas[j];
+				return c;
+			}
+			
+		}
+		return null;
+	}
 	public void creditar(int i,float v){
-		this.contas[i].creditar(v);
+		Conta c;
+		c=this.buscar(i);
+		if(c!=null){
+			c.creditar(v);
+		}else{
+			System.out.println("erro");
+		}
 	}
+	public void debitar(int i,float v){
+		Conta c=this.buscar(i);
+		if(c!=null){
+			c.debitar(v);
+		}else{
+			System.out.println("erro");
+		}
+		
+	}
+	
 	public void consultarSaldo(int i){
-		this.contas[i].consultarSaldo();
+		Conta c=this.buscar(i);
+		c.consultarSaldo();
 	}
+	
 	
 }
