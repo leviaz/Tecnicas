e5150608a7188fd73515a5edbb3355f11a32d438
leviaz <37335495+leviaz@users.noreply.github.com>
2018-03-29 12:01:36 -0300

    Update Coloridas.java

--- Coloridas.java
+++ Coloridas.java
@@ -1,7 +1,7 @@
 public interface Coloridas {
    
 
-    public abstract void desenhar(String cor);//imprimir tipo da figura
+    public abstract void desenhar(String cor);//imprimir tipo da figura e a cor passada
 	
     public abstract void mover(float dx,float dy);//mover a figura de sua origem x,y
 	
