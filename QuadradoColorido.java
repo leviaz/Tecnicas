e6385d531bb33dbd5dafaa24f0ae65350985b793
leviaz <37335495+leviaz@users.noreply.github.com>
2018-03-29 12:21:43 -0300

    Update QuadradoColorido.java

--- QuadradoColorido.java
+++ QuadradoColorido.java
@@ -1,13 +1,15 @@
 public class QuadradoColorido extends Quadrado implements Coloridas {
-
+    public String cor;
     public QuadradoColorido(float l, float x, float y) {
         super(l, x, y);
     }
-
+    
     @Override
     public void desenhar(String cor) {
-        System.out.println(this.toString()+" da cor "+cor);
+        this.cor=cor;
+        System.out.println(this.toString()+" da cor "+this.cor);
     }
            
             
 }
+
